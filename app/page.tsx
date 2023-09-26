ebc24568546ec88771546be2caa964fc1e81902f
lukasjoho <social@lukashoppe.com>
2023-09-27 01:56:57 +0200

    build: add view github link

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,16 @@ export default async function Home({ searchParams }: { searchParams: any }) {
   const products = await getProducts({ search });
   return (
     <Container className="space-y-4 py-4 md:space-y-8 md:py-8">
-      <h1 className="text-2xl font-semibold">Inventory App</h1>
+      <div>
+        <h1 className="text-2xl font-semibold">Inventory App</h1>
+        <a
+          href="https://github.com/lukasjoho/inventory-app"
+          className="cursor-pointer text-sm text-blue-500 hover:underline"
+          target="_blank"
+        >
+          View Github project
+        </a>
+      </div>
       <div className="flex flex-col justify-between gap-2 md:flex-row md:gap-8">
         <SearchInput />
         <CreateProductButton />
