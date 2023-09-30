52304204a802c7efd822c127c90a6ac9414037d3
lukasjoho <social@lukashoppe.com>
2023-09-30 11:06:57 +0200

    build: change directory in s3

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -22,7 +22,7 @@ export async function GET(request: Request) {
     const post = await s3.createPresignedPost({
       Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
       Fields: {
-        key: `experiments/${currentTimeInSeconds}-${filename}`,
+        key: `inventory-app/${currentTimeInSeconds}-${filename}`,
       },
       Expires: 60, // seconds
       Conditions: [
